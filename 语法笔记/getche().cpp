3435bcb3f1ddadfca0a0ca3d3416aec7ff92f22b
hulian425 <52779590+hulian425@users.noreply.github.com>
2019-10-18 21:58:32 +0800

    add kbhit
    the using of getche() and kbhit()

--- 语法笔记/getche().cpp
+++ 语法笔记/getche().cpp
@@ -3,4 +3,11 @@ int main
 {
   char c;
   c = getche() // 不进入缓冲区，直接发送数据
+  
+  while(1){
+      if(kbhit()){ // 当用户按下一次按键时，kbhit()返回是1
+          printf("The user presses a key.\n");
+          getch();
+        }
+    }
 }
