957e0cc60181c21571dbdad293c53c938bd87480
hulian425 <52779590+hulian425@users.noreply.github.com>
2019-10-21 21:33:44 +0800

    next_permutation全排列
    Rearranges the elements in the range [first,last) into the next lexicographically greater permutation.

---
+++ 语法笔记/next_permutation.cpp
@@ -0,0 +1,33 @@
+// 全排列
+// Rearranges the elements in the range [first,last) into the next lexicographically greater permutation.
+
+
+// next_permutation example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::next_permutation, std::sort
+
+int main () {
+  int myints[] = {1,2,3};
+
+  std::sort (myints,myints+3);
+
+  std::cout << "The 3! possible permutations with 3 elements:\n";
+  do {
+    std::cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+  } while ( std::next_permutation(myints,myints+3) );
+  // true if the function could rearrange the object as a lexicographicaly greater permutation.
+Otherwise, the function returns false 
+
+  std::cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+
+  return 0;
+}
+// Output
+// The 3! possible permutations with 3 elements:
+// 1 2 3
+// 1 3 2
+// 2 1 3
+// 2 3 1
+// 3 1 2
+// 3 2 1
+// After loop: 1 2 3
